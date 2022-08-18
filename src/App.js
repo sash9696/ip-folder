65ce99dbe2c3bc56e098494d7189d4c1352000e0
sash9696 <sahil@Sahils-MacBook-Air.local>
2022-08-18 20:36:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  async function getNetworkIP() {
+    let found = false;
+    let resolve;
+    const promise = new Promise((res) => {
+    resolve = res;
+    });
+    const pc = new RTCPeerConnection({ iceServers: [] });
+    
+    pc.addEventListener("icecandidate", (e) => {
+    if (!e.candidate || found) return;
+    resolve(e.candidate.address);
+    found = true;
+    });
+    
+    pc.createDataChannel("");
+    pc.createOffer().then((desc) => pc.setLocalDescription(desc));
+    
+    return promise;
+    }
+getNetworkIP().then(a=>console.log(a))  
   return (
     <div className="App">
       <header className="App-header">
